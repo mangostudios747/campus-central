29b49e363a4a9065f9d78b15cbbcf42ee038e70f
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-04-07 11:31:14 -0700

    catch unauthorized user

--- pages/app/courses/index.vue
+++ pages/app/courses/index.vue
@@ -2,7 +2,7 @@
 <div >
   <h1>Classes</h1>
   <v-list v-if='sections'>
-    <v-list-item v-for='section in sections' :key='section.id' two-line>
+    <v-list-item link v-for='section in sections' :key='section.id' two-line>
       <v-list-item-content>
         <v-list-item-title>{{ section.course_title }}</v-list-item-title>
         <v-list-item-subtitle>{{ section.section_title }}</v-list-item-subtitle>
