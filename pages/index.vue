397a31713e7a473e046a013a0c2b973eedf8f6c1
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-04-10 17:33:18 -0700

    fix unfinished home page, specify correct encoding for favicon

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="9">
       <div  class='text-center my-2'>
-        <img width='400' :src='$icon(512)'></img>
+        <img width='400' :src='$icon(512)'/>
         <h1 class='display-4 mb-3'>Campus Central</h1>
         <h1 class='display-2 grey--text'>All of your Schoology struggles, solved.</h1>
       </div>
-      <v-row>
+      <v-row v-if='false'>
         <v-col>
           <v-card>
             <v-system-bar/>
