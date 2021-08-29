04c9543f49551d877801a512697a1f18255d4b58
saumyasinghal747 <saumyasmathtutoring@gmail.com>
2021-08-29 03:56:00 +0000

    feat: mobile friendly schedule viewer

--- pages/app/schedule.vue
+++ pages/app/schedule.vue
@@ -1,69 +1,135 @@
 <template>
-  <div >
-    <v-row v-if='schedule.length > 0'>
-      <v-col class='mx-16'>
-        <v-progress-linear elevation='0'  :value='period.progress' height='3.5rem' class='mr-16 my-3' :color='period.color+"bb"' :key='period.hcname'  rounded  v-for='period in schedule'>
-          <v-row class='text-center px-8'>
-            <div style='align-self: center;text-align: left'>
-              <span>{{ period.status===periodStates.FUTURE?'Starting':'Started' }} {{period.start | moment('from','now')}}</span><br/>
-              <span style='color: rgba(255,255,255,0.7)'>at {{period.start | moment('h:mm A')}}</span>
+  <div class="flex flex-col">
+    <div class="px-10 flex flex-row w-full m-0">
+      <v-btn @click="prevDay" icon><v-icon>mdi-chevron-left</v-icon></v-btn>
+      <v-spacer />
+      <h1 class="hidden sm:block">
+        {{ focusedDate | moment('dddd, MMMM Do, YYYY') }}
+      </h1>
+      <h3 class="block sm:hidden my-auto">
+        {{ focusedDate | moment('ddd, MMM Do, YYYY') }}
+      </h3>
+      <v-spacer />
+      <v-btn @click="nextDay" icon><v-icon>mdi-chevron-right</v-icon></v-btn>
+    </div>
+    <v-row v-if="schedule.length > 0">
+      <v-col class="mx-10">
+        <v-progress-linear
+          elevation="0"
+          :value="period.progress"
+          height="none"
+          class="mr-16 rounded-sm h-32 md:h-14 my-3 py-2"
+          :color="period.color + 'bb'"
+          :key="period.hcname"
+          rounded
+          v-for="period in schedule"
+        >
+          <v-row class="text-center big-screen px-8">
+            <div style="align-self: center; text-align: left">
+              <span
+                >{{
+                  period.status === periodStates.FUTURE ? 'Starting' : 'Started'
+                }}
+                {{ period.start | moment('from', 'now') }}</span
+              ><br />
+              <span style="color: var(--text)"
+                >at {{ period.start | moment('h:mm A') }}</span
+              >
             </div>
-            <v-spacer/>
-            <h2 style='align-self: center'>{{ period.name }}</h2>
-            <v-spacer/>
-            <div style='align-self: center;text-align: right'>
-            <span>{{ period.status===periodStates.PAST?'Ended':'Ending' }} {{period.end | moment('from','now')}}</span><br/>
-            <span style='color: rgba(255,255,255,0.7)'>at {{period.end | moment('h:mm A')}}</span>
-          </div>
+            <v-spacer />
+            <h2 style="align-self: center">{{ period.name }}</h2>
+            <v-spacer />
+            <div style="align-self: center; text-align: right">
+              <span
+                >{{ period.status === periodStates.PAST ? 'Ended' : 'Ending' }}
+                {{ period.end | moment('from', 'now') }}</span
+              ><br />
+              <span style="color: var(--text)"
+                >at {{ period.end | moment('h:mm A') }}</span
+              >
+            </div>
           </v-row>
+          <div class="text-center flex flex-row small-screen px-3">
+            <v-col
+              ><div class="flex flex-row"
+                ><span class="font-semibold text-xl">{{ period.name }}</span></div
+              >
+              <div class="flex flex-row"><span 
+                ><span :style="`color: var(--text)`">{{
+                  period.start | moment('h:mm A')
+                }}</span> - <span :style="`color: var(--text)`">{{
+                  period.end | moment('h:mm A')
+                }}</span></span
+              ></div>
+              <div class="flex flex-row">
+                <span v-if="period.status === periodStates.FUTURE" style="color: var(--text);">
+                  Starting {{period.start | moment('from', 'now')}}
+                  </span> 
+                  <span v-else
+                >{{ period.status === periodStates.PAST ? 'Ended' : 'Ending' }}
+                {{ period.end | moment('from', 'now') }}</span
+              >
+              </div>
+            </v-col>
+          </div>
         </v-progress-linear>
       </v-col>
     </v-row>
-  <v-row class='mx-16 my-5 text-center' v-else>
-    <v-col class='mx-auto text-center'>
-      <v-icon  size='300'>mdi-sofa-single</v-icon>
-      <h1>No School Today</h1>
-    </v-col>
-
-
-
-  </v-row>
+    <v-row class="mx-16 my-5 text-center" v-else>
+      <v-col class="mx-auto text-center">
+        <v-icon class="big-screen" size="300">mdi-sofa-single</v-icon>
+        <v-icon class="small-screen" size="200">mdi-sofa-single</v-icon>
+        <h1>No School Today</h1>
+      </v-col>
+    </v-row>
   </div>
-
-
 </template>
 
 <script>
 export default {
   name: 'todo',
-  data:()=>({
-    focusedDate:(new Date),//.addDays(2),
+  data: () => ({
+    focusedDate: new Date(), //.addDays(2),
     periodStates: {
-      PAST:-1,
-      PRESENT:0,
-      FUTURE:1
+      PAST: -1,
+      PRESENT: 0,
+      FUTURE: 1,
     },
     schedule: [],
   }),
-  head(){
+  head() {
     return {
-      title:'Schedule'
+      title: 'Schedule',
     }
   },
   mounted() {
-    const v = this;
-    setInterval(()=>{
-      v.schedule = v.$store.getters['hc/scheduleForDate'](v.focusedDate);
+    const v = this
+    setInterval(() => {
+      v.schedule = v.$store.getters['hc/scheduleForDate'](v.focusedDate)
     }, 10)
   },
-  computed:{
+  computed: {
     /*schedule(){
       return this.$store.getters['hc/scheduleForDate'](this.focusedDate);
     }*/
-  }
+  },
+  methods: {
+    nextDay() {
+      this.focusedDate = this.focusedDate.addDays(1)
+    },
+    prevDay() {
+      this.focusedDate = this.focusedDate.addDays(-1)
+    },
+  },
 }
 </script>
 
 <style >
+.big-screen {
+  @apply hidden sm:flex !important;
+}
 
+.small-screen {
+  @apply sm:hidden flex !important;
+}
 </style>
