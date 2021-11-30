75dcd47f273b30352d6039efe807234d69708765
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-11-29 18:42:39 -0800

    stop tracking .nuxt pls

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['redirectMessages'] = require('../middleware/redirectMessages.js')
-middleware['redirectMessages'] = middleware['redirectMessages'].default || middleware['redirectMessages']
-
-middleware['storeUser'] = require('../middleware/storeUser.js')
-middleware['storeUser'] = middleware['storeUser'].default || middleware['storeUser']
-
-export default middleware
